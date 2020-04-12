3aca4117780888b498babf4b567b785d65cfdd0a
Homayoun <Homayoun@LAPTOP-LBTPHUJM>
2020-04-13 00:20:43 +0300

    First repo

---
+++ Caesar Cipher/src/crypto/lib/Encrypt.java
@@ -0,0 +1,5 @@
+package crypto.lib;
+
+public class Encrypt {
+
+}
---
+++ Caesar Cipher/src/crypto/lib/Encryption.java
@@ -0,0 +1,8 @@
+package crypto.lib;
+
+public class Encryption {
+	public static void main(String[] args) {
+		Decrypt de = new Decrypt();
+		Encrypt en = new Encrypt();
+	}
+}
